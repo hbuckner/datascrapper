850cac2bac4388fdd918868a89e931516a18d9cd
Hadrian Buckner <bladeastro@gmail.com>
2018-05-09 21:08:08 -0400

    added get_text function
    gets text from url
    also cleaned up get_links output

--- data_scrapper.py
+++ data_scrapper.py
@@ -12,10 +12,24 @@ def get_links(url):
     soup = bs4.BeautifulSoup(r.data,'lxml')
     for link in soup.find_all('a'):
         print(link)
-        file = open("output.txt","a")
-        #file.write("hello")
+        file = open("output.txt","a+")
+        #file.write("LINKS")
+        if (str(link.get('href'))[0:4] =="http"):
+            file.write(str(link.get('href'))+"\n")
+    print("got here")
 
-        file.write(str(link.get('href'))+"\n")
+def get_text(url):
+    manager = PoolManager()
+    r=manager.request( 'GET',url)
+    print(r)
+    #file = open("output2.txt","w")
+    soup = bs4.BeautifulSoup(r.data,'lxml')
+    for link in soup.find_all('p'):
+        print(link)
+        file = open("output.txt","a+")
+        #file.write("TEXT")
+        file.write(link.text+"\n")
     print("got here")
 #testing
 get_links("https://www.linode.com/docs/applications/big-data/how-to-scrape-a-website-with-beautiful-soup/")
+get_text("https://www.linode.com/docs/applications/big-data/how-to-scrape-a-website-with-beautiful-soup/")
